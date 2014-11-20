ef44868a39baee7ff65cf5b31de1c4d64a0ef3f4
Jimmy Wijaya <jimeh77@gmail.com>
2014-11-20 22:35:45 +1100

    just commit.

--- src/main/java/netty/smpp/traditional/SmppClient.java
+++ src/main/java/netty/smpp/traditional/SmppClient.java
@@ -1,7 +1,29 @@
 package netty.smpp.traditional;
 
-import ie.omk.smpp.AlreadyBoundException;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 
+import java.nio.charset.Charset;
 
 public class SmppClient {
+
+    public static void main(String[] args) {
+        Charset utf8 = Charset.forName("UTF-8");
+
+
+        ByteBuf buf = Unpooled.copiedBuffer("Netty in Action rocks!", utf8);
+        System.out.println((char) buf.getByte(0));
+        int readerIndex = buf.readerIndex();
+        int writerIndex = buf.writerIndex();
+
+        buf.setByte(0, (byte) 'B');
+
+        System.out.println((char) buf.getByte(0));
+
+        /*
+        assert readerIndex = buf.readerIndex();
+        assert writerIndex = buf.writerIndex();
+*/
+
+    }
 }
