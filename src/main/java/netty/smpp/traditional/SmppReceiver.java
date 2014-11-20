9eb3b0223a551b3e48a211653f798e0e9ec5755e
Jimmy Wijaya <Jimmy.Wijaya@betfair.com>
2014-11-20 14:29:57 +1100

    smpp receiver.

---
+++ src/main/java/netty/smpp/traditional/SmppReceiver.java
@@ -0,0 +1,78 @@
+package netty.smpp.traditional;
+
+import ie.omk.smpp.Connection;
+import ie.omk.smpp.event.ConnectionObserver;
+import ie.omk.smpp.event.SMPPEvent;
+import ie.omk.smpp.message.BindResp;
+import ie.omk.smpp.message.SMPPPacket;
+
+public class SmppReceiver implements ConnectionObserver {
+
+    private Connection myConnection;
+
+    private SmppReceiverConfig config = new SmppReceiverConfig();
+
+    public static void main(String... args) {
+        SmppReceiver receiver = new SmppReceiver();
+        receiver.connect();
+    }
+
+    public void connect() {
+        try {
+
+            myConnection = new Connection(config.getHost(), config.getPort(), true);
+            myConnection.addObserver(this);
+
+            // Automatically respond to ENQUIRE_LINK requests from the SMSC
+            myConnection.autoAckLink(false);
+            myConnection.autoAckMessages(false);
+
+            // Bind to the SMSC
+            System.out.println("Binding to the SMSC.");
+
+            synchronized (this) {
+                BindResp resp = myConnection.bind(
+                        Connection.RECEIVER,
+                        config.getSystemId(),
+                        config.getPassword(),
+                        config.getSystemType(),
+                        config.getBindTON(),
+                        config.getBindNPI(),
+                        config.getAddressRange());
+
+                System.out.println("Waiting for unbind...");
+                wait();
+            }
+            myConnection.closeLink();
+        } catch (Exception ex) {
+            System.out.println("Exception while receiver trying to connect: " + ex);
+        }
+    }
+
+    @Override
+    public void packetReceived(Connection myConnection, SMPPPacket smppPacket) {
+        switch (smppPacket.getCommandId()) {
+
+            // Bind transmitter response. Check it's status for success...
+            case SMPPPacket.BIND_RECEIVER_RESP:
+                if (smppPacket.getCommandStatus() != 0) {
+                    System.out.println("Error binding to the SMSC. Error = " + smppPacket.getCommandStatus());
+                } else {
+                    System.out.println("Successfully bound!");
+                }
+                break;
+
+            default:
+                System.out.println("Unexpected packet received! Id = 0x" + Integer.toHexString(smppPacket.getCommandId()));
+        }
+    }
+
+    @Override
+    public void update(Connection r, SMPPEvent ev) {
+        switch (ev.getType()) {
+            case SMPPEvent.RECEIVER_EXIT:
+                System.out.println("Receive exit");
+                break;
+        }
+    }
+}
---
+++ src/main/java/netty/smpp/traditional/SmppReceiverConfig.java
@@ -0,0 +1,99 @@
+package netty.smpp.traditional;
+
+public class SmppReceiverConfig {
+
+    private String host = "localhost";
+
+    private int port = 5555;
+
+    private String systemId = "DAMIEN";
+
+    private String password = "neimad";
+
+    private String systemType = "";
+
+    private int bindTON = 0;
+
+    private int bindNPI = 0;
+
+    private String addressRange = "1000";
+
+
+    /*
+
+sourceTON=0
+sourceNPI=0
+destinationTON=0
+destinationNPI=0
+enquireLinkPause=30000
+maximumTimesWaitForEnquireLinkResponse=60000
+timesWaitForUnbindResponse=1000
+checkFilestatusThreadSleep=2000
+deadThreadSleep=5000
+restartInMinute=30
+     */
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getSystemId() {
+        return systemId;
+    }
+
+    public void setSystemId(String systemId) {
+        this.systemId = systemId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSystemType() {
+        return systemType;
+    }
+
+    public void setSystemType(String systemType) {
+        this.systemType = systemType;
+    }
+
+    public int getBindTON() {
+        return bindTON;
+    }
+
+    public void setBindTON(int bindTON) {
+        this.bindTON = bindTON;
+    }
+
+    public int getBindNPI() {
+        return bindNPI;
+    }
+
+    public void setBindNPI(int bindNPI) {
+        this.bindNPI = bindNPI;
+    }
+
+    public String getAddressRange() {
+        return addressRange;
+    }
+
+    public void setAddressRange(String addressRange) {
+        this.addressRange = addressRange;
+    }
+}
